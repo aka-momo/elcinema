b7cfdb28ee5dda19614ba1cd0ae278c4af77b796
Ramy Aboul Naga <ramy.naga@gmail.com>
2017-03-10 11:23:14 +0200

    Fix non-stop recursion in Scapper::Movie.omdb_for

--- lib/elcinema/scrapper/movie.rb
+++ lib/elcinema/scrapper/movie.rb
@@ -53,7 +53,7 @@ module Elcinema
         title = title.split(/[\:\()]/).first.strip
 
         json = open_json(base_url: OMDB_URL, params: { t: title, y: year })
-        return omdb_for(title, year: year.to_i - 1) if json['Response'] == 'False'
+        json = open_json(base_url: OMDB_URL, params: { t: title, y: year.to_i - 1 }) if json['Response'] == 'False'
 
         {}.tap do |movie|
           sanitized    = ->(value)     { value && value != 'N/A' }
